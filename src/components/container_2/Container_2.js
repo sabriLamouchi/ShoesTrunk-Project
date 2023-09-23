37b23ede7d5c414d0162f5cb7f0df7e515675d55
sabri lamouchi <lamouchsabri@gmail.com>
2023-09-23 23:31:41 +0200

    change data to files js

--- src/components/container_2/Container_2.js
+++ src/components/container_2/Container_2.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-
-import Product from './Product'
-import pop1 from './popular_product/air-force-1-07-lx-womens-shoes.png'
-import pop2 from './popular_product/air-force-1-low-retro-mens.png'
-import pop3 from './popular_product/air-jordan-1-mid-sneaker-school.png'
-
-import pop4 from './popular_product/air-jordan-1-retro-high-og-mens.png'
-import pop5 from './popular_product/blazer-mid-77-womens-shoes.png'
-
-import pop6 from './popular_product/air-more-uptempo-big-kids-shoes.png'
-import pop7 from './popular_product/jumpman-mvp-mens-shoes.png'
-import pop8 from './popular_product/blazer-mid-77-womens-shoes.png'
+import Product from "./Product";
+import products from "./Products";
 
 import './container_2.css'
 
@@ -24,65 +14,10 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 export default function container_2(){
 
-    const products=[
-        {
-            id:1,
-            img:pop1,
-            name:'air-force-1-07-lx-womens-shoes',
-            price:'300$'
-        },
-
-        {
-            id:2,
-            img:pop2,
-            name:'air-force-1-low-retro-mens',
-            price:"350$"
-        },
-
-        {
-            id:3,
-            img:pop3,
-            name:'air-jordan-1-mid-sneaker-school',
-            price:"130$"
-        },
-
-        {
-            id:4,
-            img:pop4,
-            name:'air-jordan-1-retro-high-og-mens',
-            price:'350$'
-        },
-
-        {
-            id:5,
-            img:pop5,
-            name:'blazer-mid-77-womens-shoes',
-            price:"200$"
-        },
-        
-        {
-            id:6,
-            img:pop6,
-            name:'air-more-uptempo-big-kids-shoes',
-            price:'180$'
-        },
-        {
-            id:7,
-            img:pop7,
-            name:'jumpman-mvp-mens-shoes',
-            price:'300$'
-        },
-        {
-            id:8,
-            img:pop8,
-            name:'blazer-mid-77-womens-shoes',
-            price:'250$'
-        }
-    ]
 
    const shoes= products.map((prod)=>{
         return (
-            <SwiperSlide>     
+            <SwiperSlide style={{overflow:"visible"}}>     
                   <Product 
                     key={prod.id}
                     img={prod.img}
---
+++ src/components/container_5/products.js
@@ -0,0 +1,81 @@
+    
+    
+//     // Mens shoes:
+// import men1 from './mens shoes/air-jordan-1-mid-mens-shoes.png'
+// import men2 from './mens shoes/air-jordan-3-retro-mens-shoes.png'
+// import men3 from 
+
+// //womens shoes:
+
+// import women1 from './womens shoes/air-jordan-1-mid-womens-shoes-Kg.png'
+// import womnen2 from './womens shoes/air-max-bella-tr-5-womens-workout-shoes.png'
+// import women3 from './womens shoes/air-max-correlate-womens-shoes.png'
+
+// //kids shoes :
+
+// import kid1 from './kids shoes/air-force-1-lv8-big-kids-shoes.png'
+// import kid2 from './kids shoes/air-max-97-big-kids-shoes.png'
+// import kid3 from './kids shoes/ja-1-day-one-big-kids-basketball.png'
+    
+     const products=[
+        {
+            id:1,
+            img:require('./mens shoes/air-jordan-1-mid-mens-shoes.png'),
+            name:'air_jordan_1_mid_mens_shoes',
+            price:'300$'
+        },
+
+        {
+            id:2,
+            img:require('./mens shoes/air-jordan-3-retro-mens-shoes.png'),
+            name:'air_jordan_3_retro_mens_shoes',
+            price:"350$"
+        },
+
+        {
+            id:3,
+            img:require('./mens shoes/air-jordan-7-retro-mens-shoes.png'),
+            name:'air_jordan_7_retro_mens_shoes',
+            price:"130$"
+        },
+
+        {
+            id:4,
+            img:require('./womens shoes/air-jordan-1-mid-womens-shoes-Kg.png'),
+            name:'air_jordan_1_mid_womens_shoes_Kg',
+            price:'350$'
+        },
+
+        {
+            id:5,
+            img:require('./womens shoes/air-max-bella-tr-5-womens-workout-shoes.png'),
+            name:'air_max_bella_tr_5_womens_workout_shoes',
+            price:"200$"
+        },
+        
+        {
+            id:6,
+            img:require('./womens shoes/air-max-correlate-womens-shoes.png'),
+            name:'air_max_correlate_womens_shoes',
+            price:'180$'
+        },
+        {
+            id:7,
+            img:require('./kids shoes/air-force-1-lv8-big-kids-shoes.png'),
+            name:'air_force_1_big_kids_shoes.png',
+            price:'300$'
+        },
+        {
+            id:8,
+            img:require('./kids shoes/air-max-97-big-kids-shoes.png'),
+            name:'air_max_97_big_kids_shoes',
+            price:'250$'
+        },
+        {
+            id:9,
+            img:require('./kids shoes/ja-1-day-one-big-kids-basketball.png'),
+            name:'one_big_kids_basketball',
+            price:'250$'
+        }
+    ]
+    export default products;
