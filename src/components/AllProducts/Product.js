53f547982273b201f9c1e3a5140e71b407d207da
sabri lamouchi <lamouchsabri@gmail.com>
2023-11-18 11:27:58 +0100

    data changes

---
+++ src/components/AllProducts/Product.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react";
+import'./products.css'
+import { useCart } from "react-use-cart";
+import add_icon from './add-icon.svg'
+import { NavLink } from "react-router-dom";
+
+export default function Product(props){
+    const{addItem}=useCart(); 
+
+      const hundleProductDetailClick=()=>{
+        productDetailClicked(props.item);
+    }
+    const imgRef=useRef()
+    useEffect(()=>{
+        imgRef.current.ondblclick = (event) => {
+            hundleProductDetailClick();
+        };
+    },[])
+    return( 
+        <div 
+        className="shoes"
+        >
+            <h3>{props.name}</h3>
+            <img ref={imgRef}  src={props.img} alt={props.name}/>
+            <p className="price">{props.price}$</p>
+            <button
+                    onClick={()=>{
+                        addItem(props.item)
+                    }} 
+                    className="add-to-cart">add to cart</button>
+        </div>
+        
+
+    );
+}
+
+
+export const productDetailClicked=(item)=>{
+    return item;
+}
+
+
